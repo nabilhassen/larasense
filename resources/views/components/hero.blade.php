84b128b2cc401e815a417a6e8f017c2211c51152
Nabil Hassen <nabil.hassen08@gmail.com>
2024-11-25 19:18:30 +0300

    wip

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -5,8 +5,8 @@
                 The Only Laravel News Website You'll Ever Need
             </h1>
             <p class="max-w-3xl mx-auto py-6">
-                We will help you stay on top of the latest news, updates, and trends in the Laravel ecosystem by
-                carefully curating content from your favorite and most trusted blogs, YouTube channels, and podcasts.
+                We will help you stay on top of the latest news, updates, and trends in the Laravel ecosystem by curating content
+                from your favorite and most trusted blogs, YouTube channels, and podcasts, all presented in a simple and beautiful design.
             </p>
             <button class="btn btn-lg bg-[#EF5A6F] font-bold text-white border-none hover:bg-[#EF5A6F] hover:brightness-90">
                 Feeling behind? Join Now →
