280091434a817256bffb1be7d96485d0cfbaa8bb
Nabil Hassen <nabil.hassen08@gmail.com>
2024-11-27 14:26:03 +0300

    wip

---
+++ resources/views/components/sources.blade.php
@@ -0,0 +1,104 @@
+<div class="py-20">
+    <div class="container mx-auto space-y-12">
+        <div>
+            <h2 class="text-black font-extrabold text-center">
+                Curated content from blogs, YouTube channels, podcasts we all know and love
+            </h2>
+        </div>
+        <div class="grid sm:grid-cols-5 grid-cols-3 gap-y-12 gap-x-6 items-center justify-items-center mx-4 sm:mx-0">
+            <div>
+                <img
+                    src="{{ asset('sources/logotype.min.svg') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="scale-75"
+                    src="{{ asset('sources/secondary-logo.svg') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="size-24"
+                    src="{{ asset('sources/1.jpg') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    src="{{ asset('sources/freek.dev.png') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="size-24"
+                    src="{{ asset('sources/4.jpg') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="size-24"
+                    src="{{ asset('sources/logo.svg') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    src="{{ asset('sources/harris.png') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="size-24"
+                    src="{{ asset('sources/2.jpg') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="size-24"
+                    src="{{ asset('sources/3.jpg') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    src="{{ asset('sources/laraveldaily.png') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="w-40"
+                    src="{{ asset('sources/securinglaravel.png') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    src="{{ asset('sources/benjamincrozat.com.png') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="size-24"
+                    src="{{ asset('sources/php_foundation.png') }}"
+                    alt=""
+                >
+            </div>
+            <div>
+                <img
+                    class="w-40"
+                    src="{{ asset('sources/laravelio.png') }}"
+                    alt=""
+                >
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,13 +1,16 @@
-<div class="bg-gradient-to-b from-[#FFF1DB] pt-4">
-    <div class="container mx-auto">
-        <x-navbar />
-        <x-hero />
-        <figure class="ml-4 sm:mx-0 overflow-hidden">
-            <img
-                src="{{ asset('/img/dashboard.png') }}"
-                class="sm:rounded-box rounded-tl-box max-w-2xl sm:max-w-full"
-                alt="Dashboard"
-            >
-        </figure>
+<div>
+    <div class="bg-gradient-to-b from-[#FFF1DB] pt-4">
+        <div class="container mx-auto">
+            <x-navbar />
+            <x-hero />
+            <figure class="ml-4 sm:mx-0 overflow-hidden">
+                <img
+                    src="{{ asset('/img/dashboard.png') }}"
+                    class="sm:rounded-badge rounded-tl-badge max-w-2xl sm:max-w-full"
+                    alt="Dashboard"
+                >
+            </figure>
+        </div>
     </div>
+    <x-sources />
 </div>
