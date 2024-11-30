18a970ab59992cea0c53117f7dba7a715afe16de
Nabil Hassen <nabil.hassen08@gmail.com>
2024-11-30 14:55:18 +0300

    wip

--- resources/views/components/comparison.blade.php
+++ resources/views/components/comparison.blade.php
@@ -1,15 +1,15 @@
 <div class="container mx-auto">
-    <div class="mx-4 bg-stone-50 rounded-badge sm:px-20 px-6 py-16 space-y-20">
+    <div class="mx-4 border-4 border-[#FFF1DB] rounded-badge sm:px-20 px-6 py-16 space-y-20 shadow-xl">
         <h2 class="text-5xl text-center font-bold">
-            Stay informed like <span class="text-emerald-500">a PRO</span>
+            Stay informed like <span class="text-[#EF5A6F]">a PRO</span>
         </h2>
         <div class="grid sm:grid-cols-2 sm:gap-x-16 gap-y-16 sm:gap-y-0 sm:px-24">
-            <div class="flex flex-col items-center border border-[#EF5A6F] p-8 rounded-badge space-y-6 shadow-sm">
+            <div class="flex flex-col items-center bg-[#FFF1DB] p-8 rounded-badge space-y-6">
                 <div>
-                    <x-heroicon-o-x-circle class="size-28 stroke-[#EF5A6F]" />
+                    <x-heroicon-o-x-circle class="size-28 stroke-stone-500" />
                 </div>
                 <div class="self-start px-3">
-                    <ul class="list-disc space-y-4 text-[#EF5A6F]">
+                    <ul class="list-disc space-y-4 text-stone-500">
                         <li> Countless subscriptions to newsletter </li>
                         <li> Visiting endless websites to check for updates </li>
                         <li> Scrolling through social media full of distraction </li>
@@ -17,7 +17,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="flex flex-col items-center border bg-emerald-500 text-white p-8 rounded-badge space-y-6 shadow-sm">
+            <div class="flex flex-col items-center border bg-[#EF5A6F] p-8 rounded-badge space-y-6 shadow-lg">
                 <div>
                     <x-heroicon-o-check-circle class="size-28 stroke-white" />
                 </div>
