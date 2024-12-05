c16d3f5b1692d576d183ec8ca3054ac838732169
Nabil Hassen <nabil.hassen08@gmail.com>
2024-12-05 20:13:44 +0300

    auth: wip

--- resources/views/components/cta.blade.php
+++ resources/views/components/cta.blade.php
@@ -1,10 +1,10 @@
 <section class="container mx-auto text-stone-700">
-    <div class="bg-secondary rounded-badge sm:px-20 px-6 py-16 space-y-10 max-sm:mx-4">
+    <div class="bg-secondary rounded-badge p-12 max-sm:px-8 space-y-10 max-sm:mx-4">
         <div>
             <h2 class="sm:text-4xl text-3xl font-bold">
                 Get Your Laravel Updates On Time<span class="sm:text-5xl text-3xl">.</span>
             </h2>
-            <h3>
+            <h3 class="sm:mt-2 mt-4">
                 Curated content, beautifully presented from the best developers in the Laravel community.
             </h3>
         </div>
