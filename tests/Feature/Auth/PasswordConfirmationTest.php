05f76a14aaae877291258f6a0c6bf9227098c153
Nabil Hassen <nabil.hassen08@gmail.com>
2024-12-25 19:37:54 +0300

    wip

--- tests/Feature/Auth/PasswordConfirmationTest.php
+++ tests/Feature/Auth/PasswordConfirmationTest.php
@@ -21,7 +21,7 @@ test('password can be confirmed', function () {
         ->test(ConfirmPassword::class)
         ->set('password', 'password')
         ->call('confirmPassword')
-        ->assertRedirect('/dashboard')
+        ->assertRedirect(route('dashboard'))
         ->assertHasNoErrors();
 });
 
