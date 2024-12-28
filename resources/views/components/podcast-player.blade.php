65a3dbc317e60a02a56dcc4a97d488007e8f2a49
Nabil Hassen <nabil.hassen08@gmail.com>
2024-12-28 20:12:21 +0300

    refactor podcast player

---
+++ resources/views/components/podcast-player.blade.php
@@ -0,0 +1,26 @@
+@props(['material'])
+
+<div
+    x-data="podcastPlayer"
+    x-on:close-podcast-modal.window="continueOnMainPodcastPlayer($event.detail)"
+    class="flex items-center bg-secondary rounded-btn p-4"
+>
+    <figure class="w-1/5">
+        <img
+            src="{{ $material->thumbnail }}"
+            alt=""
+            class="rounded-box size-full"
+        >
+    </figure>
+    <div class="podcast-player w-full">
+        <audio
+            class="player"
+            controls
+        >
+            <source
+                src="{{ $material->url }}"
+                type="audio/mp3"
+            />
+        </audio>
+    </div>
+</div>
--- resources/views/components/topnavbar.blade.php
+++ resources/views/components/topnavbar.blade.php
@@ -61,27 +61,6 @@
             </div>
         </div>
     </div>
-    <div
-        x-cloak
-        x-data="mainPodcastPlayer"
-        x-show="isSourceSet"
-        x-on:play-podcast.window="play($event.detail)"
-        x-on:pause-podcast.window="player.pause()"
-        class="lg:w-6/12 h-16 flex items-center p-4 rounded-btn bg-secondary"
-    >
-        <figure>
-            <img
-                x-bind:src="thumbnail"
-                alt=""
-                class="rounded max-h-12"
-            >
-        </figure>
-        <div id="main-podcast-player-container">
-            <audio
-                id="main-podcast-player"
-                controls
-            >
-            </audio>
-        </div>
-    </div>
+
+    <x-main-podcast-player />
 </div>
