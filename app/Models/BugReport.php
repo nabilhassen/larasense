554241bd4f376b3601bd24e8db952664d264e313
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-02 16:58:56 +0300

    bug reports table

---
+++ app/Models/BugReport.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class BugReport extends Model
+{
+    /** @use HasFactory<\Database\Factories\BugReportFactory> */
+    use HasFactory;
+
+    public function publisher(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
