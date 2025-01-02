eff9e70b5e0a8329e68b799fd192fb12743c69b5
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-02 17:06:25 +0300

    bug reports filament

---
+++ app/Filament/Resources/BugReportResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\BugReportResource\Pages;
+use App\Models\BugReport;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Actions\BulkActionGroup;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class BugReportResource extends Resource
+{
+    protected static ?string $model = BugReport::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-bug';
+
+    protected static ?int $navigationSort = 4;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('user_id')
+                    ->relationship('user', 'name'),
+
+                TextInput::make('description')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable(),
+
+                TextColumn::make('description')
+                    ->lineClamp(2)
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable(),
+
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable(),
+
+            ])
+            ->actions([
+                ViewAction::make(),
+                DeleteAction::make(),
+            ])
+            ->bulkActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageBugReports::route('/'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/BugReportResource/Pages/ManageBugReports.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\BugReportResource\Pages;
+
+use App\Filament\Resources\BugReportResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageBugReports extends ManageRecords
+{
+    protected static string $resource = BugReportResource::class;
+}
