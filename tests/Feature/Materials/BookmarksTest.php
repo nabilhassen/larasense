34e1768133bf9715f6896f9256e2cdfbe56bc3c6
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-02 22:42:36 +0300

    test materials bookmarks component

---
+++ tests/Feature/Materials/BookmarksTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Livewire\Materials\Bookmarks;
+use App\Livewire\Materials\Show;
+use App\Models\Material;
+use App\Models\User;
+use Livewire\Livewire;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+
+    $this->actingAs($this->user);
+});
+
+test('bookmarks page renders successfully', function () {
+    $response = $this->get(route('bookmarks'));
+
+    $response->assertStatus(200);
+});
+
+test('bookmarks feed displays bookmarked materials only', function () {
+    Material::factory(10)->create();
+    $material = Material::feedQuery()->first();
+
+    Livewire::test(Show::class, ['material' => $material])
+        ->call('bookmark');
+
+    Livewire::test(Bookmarks::class)
+        ->assertStatus(200)
+        ->assertViewHas('materials', function ($materials) use ($material) {
+            return $materials->contains($material->id);
+        });
+});
+
+test('bookmarks feed can load more bookmarked materials', function () {
+    Material::factory(20)->create();
+
+    foreach (Material::feedQuery()->limit(10)->get() as $material) {
+        Livewire::test(Show::class, ['material' => $material])
+            ->call('bookmark');
+    }
+
+    Livewire::test(Bookmarks::class)
+        ->assertStatus(200)
+        ->assertViewHas('materials', function ($materials) {
+            return count($materials->items()) === 6;
+        })
+        ->call('loadMore')
+        ->assertViewHas('materials', function ($materials) {
+            return count($materials->items()) === 10;
+        });
+});
