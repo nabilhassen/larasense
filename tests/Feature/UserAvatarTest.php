46478fd3184522e45dbe48d1fc5d57104e31f806
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-03 16:06:42 +0300

    test user avatar

---
+++ tests/Feature/UserAvatarTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Livewire\UserAvatar;
+use App\Models\User;
+use Livewire\Livewire;
+
+test('user avatar is pulled from ui-avatars if user has no an avatar', function () {
+    $user = User::factory()->create(['avatar_url' => null]);
+
+    Livewire::actingAs($user)
+        ->test(UserAvatar::class)
+        ->assertSeeHtmlInOrder([
+            'ui-avatars.com',
+            str($user->name)->squish()->words(2),
+        ]);
+});
+
+test('user avatar changes when the avatar is updated', function () {
+    $user = User::factory()->create();
+
+    $component = Livewire::actingAs($user)
+        ->test(UserAvatar::class)
+        ->assertSee($user->avatar);
+
+    $newImage = fake()->imageUrl();
+    $user->avatar_url = $newImage;
+    $user->save();
+    $user->refresh();
+
+    $component
+        ->refresh()
+        ->assertSee($newImage);
+});
