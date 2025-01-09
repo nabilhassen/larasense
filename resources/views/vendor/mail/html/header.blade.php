5d52b816a6de53473ac3f0ca2a1573e4767cf426
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-09 16:32:19 +0300

    customize mail theme

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ asset('img/logo.png') }}" class="logo" alt="Laravel Logo">
+</a>
+</td>
+</tr>
