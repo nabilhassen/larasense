de5a781b7a6de425a9fa6d3a8eebad3cbe2302a8
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-10 22:47:52 +0300

    escape material title prop

--- resources/views/livewire/materials/show.blade.php
+++ resources/views/livewire/materials/show.blade.php
@@ -177,7 +177,7 @@
                                 url: '{{ $material->url }}',
                                 thumbnail: '{{ $material->thumbnail }}',
                                 publisherName: '{{ $material->source->publisher->name }}',
-                                materialTitle: '{!! $material->title !!}',
+                                materialTitle: @js($material->title),
                                 publishedAt: '{{ $material->published_at->inUserTimezone()->toFormattedDateString() }}',
                                 duration: '{{ Carbon\CarbonInterval::seconds($material->duration)->cascade()->forHumans(['short' => true]) }}',
                             });
