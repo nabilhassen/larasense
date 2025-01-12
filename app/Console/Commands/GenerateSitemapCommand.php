8d656c35d3b96f0cb5d3ce5ed7a8cb11c3831cfd
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-12 19:37:33 +0300

    create sitemap generator command

---
+++ app/Console/Commands/GenerateSitemapCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Psr\Http\Message\UriInterface;
+use Spatie\Sitemap\SitemapGenerator;
+
+class GenerateSitemapCommand extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the sitemap.';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        SitemapGenerator::create(config('app.url'))
+            ->shouldCrawl(function (UriInterface $url) {
+                // All pages will be crawled, except the contact page.
+                // Links present on the contact page won't be added to the
+                // sitemap unless they are present on a crawlable page.
+
+                return !str($url->getPath())
+                    ->contains([
+                        '/auth',
+                        'password',
+                    ]);
+            })
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
