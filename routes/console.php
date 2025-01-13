d245819c12366b136b3dd3f35cdfb818ae741312
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-13 22:03:27 +0300

    remove pulse check command

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,4 @@ Schedule::command('queue-monitor:purge --beforeDays=7')->withoutOverlapping()->d
 
 Schedule::command('larasense:bot')->withoutOverlapping()->everyTenMinutes();
 
-Schedule::command('pulse:check')->withoutOverlapping()->everyMinute();
-
 Schedule::command('queue:work --tries=3 --backoff=3 --stop-when-empty')->withoutOverlapping()->everySecond();
