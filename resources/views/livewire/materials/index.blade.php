d23c4b9c54178b8aeedfea6811709fabe2dad029
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-13 20:21:12 +0300

    remove shuffle method

--- resources/views/livewire/materials/index.blade.php
+++ resources/views/livewire/materials/index.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 lg:gap-x-4 gap-y-8">
-        @foreach ($materials->shuffle() as $material)
+        @foreach ($materials as $material)
             <livewire:materials.show
                 :slug="$material->slug"
                 wire:key="material-{{ $material->slug }}"
