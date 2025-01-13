98be62008ccc95438afdec3c7b73695ebcd98e35
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-13 12:43:48 +0300

    change uuid to bigincrements for queue-monitor table

--- database/migrations/2018_02_05_000000_create_queue_monitor_table.php
+++ database/migrations/2018_02_05_000000_create_queue_monitor_table.php
@@ -21,7 +21,7 @@ class CreateQueueMonitorTable extends Migration
     {
         Schema::create(config('queue-monitor.table'), function (Blueprint $table) {
             $table->increments('id');
-            $table->uuid('job_uuid')->nullable();
+            $table->bigIncrements('job_uuid')->nullable();
 
             $table->string('job_id')->index();
             $table->string('name')->nullable();
