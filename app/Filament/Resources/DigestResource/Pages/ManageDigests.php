26c13ea3aa034330b58afb123a9031e9d1c9f20a
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-30 20:45:21 +0300

    create digests filament resource

---
+++ app/Filament/Resources/DigestResource/Pages/ManageDigests.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\DigestResource\Pages;
+
+use App\Filament\Resources\DigestResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageDigests extends ManageRecords
+{
+    protected static string $resource = DigestResource::class;
+}
