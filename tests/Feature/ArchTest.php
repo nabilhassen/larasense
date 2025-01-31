d1a83a2a4b047c457e398856f4bb4b1d00b58d8d
Nabil Hassen <nabil.hassen08@gmail.com>
2025-01-31 17:57:15 +0300

    organize directories

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -1,11 +1,9 @@
 <?php
 
 use App\Livewire\Bookmarks;
+use App\Livewire\FeedBySourceType;
 use App\Livewire\Likes;
-use App\Livewire\Materials\Card;
 use App\Livewire\Materials\Index;
-use App\Livewire\Materials\Modal;
-use App\Livewire\Materials\Show;
 use App\Livewire\Traits\CanLoadMore;
 use App\Livewire\Traits\HasEngagementMetrics;
 
@@ -29,16 +27,14 @@ arch('material components uses HasEngagementMetrics trait')
     ->expect('App\Livewire\Materials')
     ->toUseTrait(HasEngagementMetrics::class)
     ->ignoring([
-        Bookmarks::class,
         Index::class,
-        Likes::class,
     ]);
 
-arch('material components uses CanLoadMore trait')
-    ->expect('App\Livewire\Materials')
-    ->toUseTrait(CanLoadMore::class)
-    ->ignoring([
-        Card::class,
-        Modal::class,
-        Show::class,
-    ]);
+arch('components uses CanLoadMore trait')
+    ->expect([
+        Index::class,
+        Bookmarks::class,
+        FeedBySourceType::class,
+        Likes::class,
+    ])
+    ->toUseTrait(CanLoadMore::class);
