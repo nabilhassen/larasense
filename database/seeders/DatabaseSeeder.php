f797d36112c3c73adbd69c1570d977133b2651ec
Nabil Hassen <nabil.hassen08@gmail.com>
2025-07-07 20:23:24 +0300

    refactor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,10 +5,8 @@ namespace Database\Seeders;
 use App\Models\Material;
 use App\Models\Publisher;
 use App\Models\Source;
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,10 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Publisher::factory(5)
+        Publisher::factory()
+            ->count(5)
             ->has(
                 Source::factory(2)
-                    ->has(Material::factory(5))
+                ->has(Material::factory()->count(5))
             )
             ->create();
     }
