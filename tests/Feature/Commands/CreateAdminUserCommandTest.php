1ab19a7a6669b4bbfbcf026db5dae35a535658bb
Nabil Hassen <nabil.hassen08@gmail.com>
2025-07-10 20:46:44 +0300

    fix create admin user command

--- tests/Feature/Commands/CreateAdminUserCommandTest.php
+++ tests/Feature/Commands/CreateAdminUserCommandTest.php
@@ -9,22 +9,6 @@ test('command can create admin user', function () {
 
     $this->assertDatabaseCount('users', 1);
     $this->assertDatabaseHas('users', [
-        'email' => 'admin@larasense.com',
-    ]);
-});
-
-test('command can create admin user only once', function () {
-    artisan('make:admin')->assertSuccessful();
-
-    $this->assertDatabaseCount('users', 1);
-    $this->assertDatabaseHas('users', [
-        'email' => 'admin@larasense.com',
-    ]);
-
-    artisan('make:admin')->assertSuccessful();
-
-    $this->assertDatabaseCount('users', 1);
-    $this->assertDatabaseHas('users', [
-        'email' => 'admin@larasense.com',
+        'is_admin' => 1,
     ]);
 });
