adbca7996438790355e9f9cb04bbac6f46cec339
Nabil Hassen <nabil.hassen08@gmail.com>
2025-08-08 23:55:13 +0300

    chore: remove essentials package

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,9 +5,17 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Sleep;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,5 +39,60 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('viewPulse', function (User $user) {
             return $user->isAdmin();
         });
+
+        $this->configureVite();
+
+        $this->configureModels();
+
+        $this->configureTests();
+
+        $this->configureHttps();
+
+        $this->configureDates();
+
+        $this->configureCommands();
+
+        $this->configurePasswordValidation();
+    }
+
+    protected function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
+    }
+
+    protected function configureModels(): void
+    {
+        Model::automaticallyEagerLoadRelationships();
+
+        Model::shouldBeStrict();
+
+        Model::unguard();
+    }
+
+    protected function configureTests(): void
+    {
+        Sleep::fake();
+    }
+
+    protected function configureHttps(): void
+    {
+        URL::forceHttps(app()->isProduction());
+    }
+
+    protected function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
+    }
+
+    protected function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            app()->isProduction(),
+        );
+    }
+
+    protected function configurePasswordValidation(): void
+    {
+        Password::defaults(fn (): ?Password => app()->isProduction() ? Password::min(12)->max(255)->uncompromised() : null);
     }
 }
