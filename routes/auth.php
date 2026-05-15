0da09d56e6dcd0ea39286babea55a21ca672ae4f
Nabil Hassen <nabil.hassen08@gmail.com>
2026-05-15 08:12:11 +0300

    livewire v4 upgrade

--- routes/auth.php
+++ routes/auth.php
@@ -9,29 +9,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')
     ->group(function () {
-        Route::get('verify-email', Auth\VerifyEmail::class)
+        Route::livewire('verify-email', Auth\VerifyEmail::class)
             ->name('verification.notice');
 
         Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
             ->middleware(['signed', 'throttle:6,1'])
             ->name('verification.verify');
 
-        Route::get('confirm-password', Auth\ConfirmPassword::class)
+        Route::livewire('confirm-password', Auth\ConfirmPassword::class)
             ->name('password.confirm');
     });
 
 Route::middleware('guest')
     ->group(function () {
-        Route::get('register', Auth\Register::class)
+        Route::livewire('register', Auth\Register::class)
             ->name('register');
 
-        Route::get('login', Auth\Login::class)
+        Route::livewire('login', Auth\Login::class)
             ->name('login');
 
-        Route::get('forgot-password', Auth\ForgotPassword::class)
+        Route::livewire('forgot-password', Auth\ForgotPassword::class)
             ->name('password.request');
 
-        Route::get('reset-password/{token}', Auth\ResetPassword::class)
+        Route::livewire('reset-password/{token}', Auth\ResetPassword::class)
             ->name('password.reset');
 
         Route::get('auth/{provider}/redirect', [SocialiteController::class, 'redirect'])
--- routes/web.php
+++ routes/web.php
@@ -10,31 +10,31 @@ require __DIR__.'/auth.php';
 
 Route::view('/', 'home')->name('home')->middleware('guest');
 
-Route::get('home', Livewire\Materials\Index::class)->name('materials.index');
+Route::livewire('home', Livewire\Materials\Index::class)->name('materials.index');
 
 Route::view('terms-and-conditions', 'terms')->name('terms');
 
 Route::view('privacy-policy', 'privacy-policy')->name('privacy');
 
-Route::get('feed/{type}', Livewire\FeedBySourceType::class)
+Route::livewire('feed/{type}', Livewire\FeedBySourceType::class)
     ->name('feed.type');
 
 Route::post('update-timezone', UpdateUserTimezoneController::class)->name('timezone.update');
 
 Route::middleware(['auth', 'verified'])
     ->group(function () {
-        Route::get('m/{slug}', Livewire\Materials\Show::class)
+        Route::livewire('m/{slug}', Livewire\Materials\Show::class)
             ->name('materials.show');
 
-        Route::get('likes', Livewire\Materials\Likes::class)
+        Route::livewire('likes', Livewire\Materials\Likes::class)
             ->name('likes');
 
-        Route::get('bookmarks', Livewire\Materials\Bookmarks::class)
+        Route::livewire('bookmarks', Livewire\Materials\Bookmarks::class)
             ->name('bookmarks');
 
         Route::view('settings', 'profile')
             ->name('settings');
 
-        Route::get('publishers/{slug}', Livewire\Publishers\Show::class)
+        Route::livewire('publishers/{slug}', Livewire\Publishers\Show::class)
             ->name('publishers.show');
     });
