86e755110a7b62ee704631c267f588fdabad82dc
Nabil Hassen <nabil.hassen08@gmail.com>
2026-05-20 09:09:06 +0300

    daisy-ui changes

--- resources/views/livewire/auth/verify-email.blade.php
+++ resources/views/livewire/auth/verify-email.blade.php
@@ -26,7 +26,7 @@
 
         <div class="flex items-center justify-between mt-8!">
             <button
-                class="btn bg-primary border-none text-white hover:bg-primary hover:brightness-90 disabled:bg-primary disabled:opacity-70 disabled:text-white"
+                class="btn shadow-none bg-primary border-none text-white hover:bg-primary hover:brightness-90 disabled:bg-primary disabled:opacity-70 disabled:text-white"
                 wire:click="sendVerification"
             >
                 Resend Verification Email
