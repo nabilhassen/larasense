2ac47548b4c5fcec7709b18224636f069868bc5a
Nabil Hassen <nabil.hassen08@gmail.com>
2026-05-26 08:32:29 +0300

    refactor modal component to use DaisyUI dialog

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -6,79 +6,50 @@
 
 @php
 $maxWidth = [
-    'sm' => 'sm:max-w-sm',
-    'md' => 'sm:max-w-md',
-    'lg' => 'sm:max-w-lg',
-    'xl' => 'sm:max-w-xl',
-    '2xl' => 'sm:max-w-2xl',
+    'sm' => 'max-w-sm',
+    'md' => 'max-w-md',
+    'lg' => 'max-w-lg',
+    'xl' => 'max-w-xl',
+    '2xl' => 'max-w-2xl',
 ][$maxWidth];
 @endphp
 
-<div
+<dialog
+    id="{{ $name }}"
+    class="modal cursor-auto"
     x-data="{
-        show: @js($show),
         focusables() {
-            // All focusable element types...
             let selector = 'a, button, input:not([type=\'hidden\']), textarea, select, details, [tabindex]:not([tabindex=\'-1\'])'
             return [...$el.querySelectorAll(selector)]
-                // All non-disabled elements...
                 .filter(el => ! el.hasAttribute('disabled'))
         },
         firstFocusable() { return this.focusables()[0] },
-        lastFocusable() { return this.focusables().slice(-1)[0] },
-        nextFocusable() { return this.focusables()[this.nextFocusableIndex()] || this.firstFocusable() },
-        prevFocusable() { return this.focusables()[this.prevFocusableIndex()] || this.lastFocusable() },
-        nextFocusableIndex() { return (this.focusables().indexOf(document.activeElement) + 1) % (this.focusables().length + 1) },
-        prevFocusableIndex() { return Math.max(0, this.focusables().indexOf(document.activeElement)) -1 },
+        open() {
+            if (! $el.open) {
+                $el.showModal()
+            }
+
+            {{ $attributes->has('focusable') ? 'setTimeout(() => this.firstFocusable()?.focus(), 100)' : '' }}
+        },
+        close() {
+            if ($el.open) {
+                $el.close()
+            }
+        },
     }"
-    x-init="$watch('show', value => {
-        if (value) {
-            document.body.classList.add('overflow-y-hidden');
-            {{ $attributes->has('focusable') ? 'setTimeout(() => firstFocusable().focus(), 100)' : '' }}
-        } else {
-            document.body.classList.remove('overflow-y-hidden');
-        }
-    })"
-    x-on:open-modal.window="$event.detail == '{{ $name }}' ? show = true : null"
-    x-on:close-modal.window="$event.detail == '{{ $name }}' ? show = false : null"
-    x-on:close.stop="show = false"
-    x-on:keydown.escape.window="show = false"
-    x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
-    x-on:keydown.shift.tab.prevent="prevFocusable().focus()"
-    x-show="show"
-    class="fixed inset-0 z-50 overflow-y-auto px-4 py-6 sm:px-0"
-    style="display: {{ $show ? 'block' : 'none' }};"
+    x-init="@js($show) && $nextTick(() => open())"
+    x-on:open-modal.window="$event.detail === '{{ $name }}' && open()"
+    x-on:close-modal.window="$event.detail === '{{ $name }}' && close()"
+    x-on:close.stop="close()"
 >
-    <div
-        x-show="show"
-        class="fixed inset-0 z-0 transform transition-all"
-        x-on:click="show = false"
-        x-transition:enter="ease-out duration-300"
-        x-transition:enter-start="opacity-0"
-        x-transition:enter-end="opacity-100"
-        x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100"
-        x-transition:leave-end="opacity-0"
-    >
-        <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+    <div class="modal-box w-full {{ $maxWidth }} dark:bg-black dark:border dark:border-stone-800">
+        {{ $slot }}
     </div>
 
-    <div
-        x-show="show"
-        class="relative z-10 flex min-h-full items-center justify-center transform transition-all"
-        x-on:click="show = false"
-        x-transition:enter="ease-out duration-300"
-        x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-        x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    <form
+        method="dialog"
+        class="modal-backdrop backdrop-blur-xs"
     >
-        <div
-            class="mb-6 w-full bg-white dark:bg-black rounded-lg overflow-hidden shadow-xl sm:w-full {{ $maxWidth }} dark:border dark:border-stone-800"
-            x-on:click.stop
-        >
-            {{ $slot }}
-        </div>
-    </div>
-</div>
+        <button>close</button>
+    </form>
+</dialog>
